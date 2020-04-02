547165179e47ab4572fe8bc102d5711b70a1eb00
Dmitry <petropavlovd@gmail.com>
2020-04-02 15:42:29 +0300

    Hello World

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SNAKE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
